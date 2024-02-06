803098b3190b339076d2bc6ab5a8f602a97cbcb2
crazypraveen11 <cspraveensenthilm@gmail.com>
2024-02-06 11:18:15 +0530

    last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
-function App() {
+let Design = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+          <div className="row justify-content-center mt-3">
+          <h1 className="text-center fst-italic ms-5"><ins><span style={{color: 'red'}}>STUDENT </span>DETAILS</ins></h1>
+          <br></br>
+      <form className=" row align-items-center  ms-3 col-lg-3 mt-4 me-4"> 
+          <label className="form-label fw-bold">Name : </label>
+          <input type='text' className="form-control ms-5" placeholder="Enter your Name"></input>
+          <br></br><br></br>
+          <label className="form-label fw-bold">Age : </label>
+          <input type='number' className="form-control ms-5" placeholder="Enter your Age"></input>
+           <br></br><br></br>
+          <label className="form-label fw-bold">Email : </label>
+          <input type='email' className="form-control ms-5" placeholder="Enter your Email"></input>
+          <br></br><br></br>
+          <label className="form-label fw-bold">Address : </label>
+          <textarea className="form-control ms-5" placeholder="Enter your Address"></textarea>
+          <br></br><br></br>
+          <label className="form-label fw-bold">Phone Number : </label>
+          <input type='number' className="form-control ms-5" placeholder="Enter your Phone Number"></input>
+          <br></br><br></br>
+          <label className="form-label fw-bold">PAN Number : </label>
+          <input type='number' className="form-control ms-5" placeholder="Enter your PAN Number"></input>
+          <br></br>
+          <div className="text-center mt-5">
+          <button className="btn btn-success text-center">SUBMIT</button>
+          </div>
+      </form>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default Design;
