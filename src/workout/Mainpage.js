7b9db09516dc0197b3770441c3361f1e7d056752
crazypraveen11 <cspraveensenthilm@gmail.com>
2024-02-23 11:27:01 +0530

    last

--- src/workout/Mainpage.js
+++ src/workout/Mainpage.js
@@ -1,42 +1,41 @@
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import { useState } from 'react'
-// import Design from '../form';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
+import Design from '../form';
 
-// export let Page = () =>
-// {
-//     var [createpage, setCreatepage] = useState(false);
+export let Page = () =>
+{
+    var [createpage, setCreatepage] = useState(false);
 
 
-//     return (
-//         <>
-//             {
-//                 (createpage) ?
-//                     <>
-//                         < Design />
-//                         <button
-//                             className='btn btn-outline-success'
-//                             onClick={
-//                                 () => {
-//                                     setCreatepage(false)
-//                                 }
-//                             }>
-//                             BACK
-//                         </button>
-//                     </>
-//                     :
-//                     <>
-//                         <button
-//                             className='btn btn-outline-success mt-5 ms-5'
-//                             onClick={
-//                                 () => {
-//                                     setCreatepage(true)
-//                                 }
-//                             }>
-//                             REGISTER
-//                         </button>
-//                     </>
-
-//             }
-//         </>
-//     )
-// }
+    return (
+        <>
+            {
+                (createpage) ?
+                    <>
+                        < Design />
+                        <button
+                            className='btn btn-outline-success'
+                            onClick={
+                                () => {
+                                    setCreatepage(false)
+                                }
+                            }>
+                            BACK
+                        </button>
+                    </>
+                    :
+                    <>
+                        <button
+                            className='btn btn-outline-success mt-5 ms-5'
+                            onClick={
+                                () => {
+                                    setCreatepage(true)
+                                }
+                            }>
+                            REGISTER
+                        </button>
+                    </>
+            }
+        </>
+    )
+}
